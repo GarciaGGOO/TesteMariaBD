9cac1b1f9b8afa5e0abd5c63048697a775fb110b
Guilherme Garcia <guilherme.garciaortega@gmail.com>
2024-06-12 12:38:12 -0300

    first commit

---
+++ TesteMariaDB/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TesteMariaDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnPopular = new System.Windows.Forms.Button();
+            this.txtResultados = new System.Windows.Forms.TextBox();
+            this.lblCronometro = new System.Windows.Forms.Label();
+            this.cronometro = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblContador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(78, 119);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(35, 13);
+            this.lblResultado.TabIndex = 0;
+            this.lblResultado.Text = "label1";
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(81, 40);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(64, 20);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            this.btnConectar.Click += new System.EventHandler(this.btnConectar_Click);
+            // 
+            // btnPopular
+            // 
+            this.btnPopular.Location = new System.Drawing.Point(81, 79);
+            this.btnPopular.Name = "btnPopular";
+            this.btnPopular.Size = new System.Drawing.Size(64, 20);
+            this.btnPopular.TabIndex = 2;
+            this.btnPopular.Text = "Popular";
+            this.btnPopular.UseVisualStyleBackColor = true;
+            this.btnPopular.Click += new System.EventHandler(this.btnPopular_Click);
+            // 
+            // txtResultados
+            // 
+            this.txtResultados.Location = new System.Drawing.Point(81, 156);
+            this.txtResultados.Multiline = true;
+            this.txtResultados.Name = "txtResultados";
+            this.txtResultados.ReadOnly = true;
+            this.txtResultados.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResultados.Size = new System.Drawing.Size(258, 87);
+            this.txtResultados.TabIndex = 3;
+            // 
+            // lblCronometro
+            // 
+            this.lblCronometro.AutoSize = true;
+            this.lblCronometro.Location = new System.Drawing.Point(81, 139);
+            this.lblCronometro.Name = "lblCronometro";
+            this.lblCronometro.Size = new System.Drawing.Size(70, 13);
+            this.lblCronometro.TabIndex = 4;
+            this.lblCronometro.Text = "00:00:00.000";
+            // 
+            // cronometro
+            // 
+            this.cronometro.Interval = 10;
+            this.cronometro.Tick += new System.EventHandler(this.cronometro_Tick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(81, 260);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(257, 20);
+            this.progressBar.TabIndex = 5;
+            // 
+            // lblContador
+            // 
+            this.lblContador.AutoSize = true;
+            this.lblContador.Location = new System.Drawing.Point(81, 283);
+            this.lblContador.Name = "lblContador";
+            this.lblContador.Size = new System.Drawing.Size(43, 13);
+            this.lblContador.TabIndex = 6;
+            this.lblContador.Text = "(0 de 0)";
+            this.lblContador.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 335);
+            this.Controls.Add(this.lblContador);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblCronometro);
+            this.Controls.Add(this.txtResultados);
+            this.Controls.Add(this.btnPopular);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.lblResultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnPopular;
+        private System.Windows.Forms.TextBox txtResultados;
+        private System.Windows.Forms.Label lblCronometro;
+        private System.Windows.Forms.Timer cronometro;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblContador;
+    }
+}
+
---
+++ TesteMariaDB/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace TesteMariaDB
+{
+    public partial class Form1 : Form
+    {
+        private DateTime startTime;
+        private bool populating = false;
+        private System.Windows.Forms.Timer formTimer;
+        private string nomeTabela = "dados2";
+        private string nomeBanco = "armazenamentobd";
+        private int totalInsercoes = 0; // Variável para acompanhar o número total de inserções
+        private int insercoesConcluidas = 0; // Variável para acompanhar o número de inserções concluídas
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            formTimer = new System.Windows.Forms.Timer();
+            formTimer.Interval = 100;
+            formTimer.Tick += cronometro_Tick;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblResultado.Text = "Aguardando conexão...";
+            lblCronometro.Text = "00:00:00.000";
+            progressBar.Minimum = 0;
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            string conexao = GetConnectionString();
+            try
+            {
+                using (var conexaoConnection = new MySqlConnection(conexao))
+                {
+                    conexaoConnection.Open();
+                    lblResultado.Text = "Conectado";
+                }
+            }
+            catch (MySqlException ex)
+            {
+                lblResultado.Text = $"Erro: {ex.Message}";
+            }
+        }
+
+        private async void btnPopular_Click(object sender, EventArgs e)
+        {
+            if (populating)
+            {
+                MessageBox.Show("População em andamento. Por favor, aguarde.");
+                return;
+            }
+
+            populating = true;
+            startTime = DateTime.UtcNow;
+            formTimer.Start();
+
+            string conexao = GetConnectionString();
+
+            try
+            {
+                using (var conexaoConnection = new MySqlConnection(conexao))
+                {
+                    conexaoConnection.Open();
+                    totalInsercoes = 5 * 365 * 1440 * 4 * 13; // Total de inserções
+                    //totalInsercoes = 100;
+                    progressBar.Maximum = totalInsercoes; // Definindo o máximo da barra de progresso
+
+                    await Task.Run(() => PopularDados(conexaoConnection));
+                    AtualizarLabel(lblResultado, "Tabela populada com sucesso.");
+                    MostrarResultados(conexaoConnection);
+                }
+            }
+            catch (MySqlException ex)
+            {
+                AtualizarLabel(lblResultado, $"Erro: {ex.Message}");
+            }
+            finally
+            {
+                populating = false;
+                formTimer.Stop();
+            }
+        }
+
+        private void PopularDados(MySqlConnection conexaoConnection)
+        {
+            Random random = new Random();
+
+            int totalEstacoes = 4;
+            int totalCameras = 13;
+            int totalMinutos = 5 * 365 * 1440;
+            int totalIteracoes = totalEstacoes * totalCameras * totalMinutos;
+            //int totalIteracoes = 100;
+
+            using (var transaction = conexaoConnection.BeginTransaction())
+            {
+                for (int i = 0; i < totalIteracoes; i++)
+                {
+                    int estacao = (i / (totalCameras * totalMinutos)) % totalEstacoes + 1;
+                    int camera = (i / totalMinutos) % totalCameras + 1;
+                    int minuto = i % totalMinutos;
+
+                    DateTime data_hora = new DateTime(2024, 1, 1, 0, 0, 0).AddMinutes(minuto);
+                    double temp_maxima = Math.Round(random.NextDouble() * 999.9, 1);
+                    double temp_minima = Math.Round(random.NextDouble() * 999.9, 1);
+                    double temp_media = Math.Round((temp_maxima + temp_minima) / 2, 1);
+                    double temp_externa = Math.Round(random.NextDouble() * 999.9, 1);
+                    DateTime hora_final = DateTime.UtcNow;
+
+                    string query = $"INSERT INTO {nomeTabela} (id_area, data_hora, temp_maxima, temp_minima, temp_media, temp_externa, hora_final) " +
+                                   "VALUES (@id_area, @data_hora, @temp_maxima, @temp_minima, @temp_media, @temp_externa, @hora_final)";
+
+                    using (var cmd = new MySqlCommand(query, conexaoConnection, transaction))
+                    {
+                        cmd.Parameters.AddWithValue("@id_area", estacao);
+                        cmd.Parameters.AddWithValue("@data_hora", data_hora);
+                        cmd.Parameters.AddWithValue("@temp_maxima", temp_maxima);
+                        cmd.Parameters.AddWithValue("@temp_minima", temp_minima);
+                        cmd.Parameters.AddWithValue("@temp_media", temp_media);
+                        cmd.Parameters.AddWithValue("@temp_externa", temp_externa);
+                        cmd.Parameters.AddWithValue("@hora_final", hora_final);
+
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    insercoesConcluidas++; // Incrementando o número de inserções concluídas
+                    AtualizarProgressBar(insercoesConcluidas); // Atualizando a barra de progresso
+                    AtualizarLabel(lblContador, $"{insercoesConcluidas} de {totalInsercoes}"); // Atualizando o contador
+                }
+
+                transaction.Commit();
+            }
+        }
+
+        private void MostrarResultados(MySqlConnection conexaoConnection)
+        {
+            AtualizarTextBox(txtResultados, "");
+
+            // Consulta para a última hora
+            string queryUltimaHora = $"SELECT COUNT(*) FROM {nomeTabela} WHERE data_hora >= NOW() - INTERVAL 1 HOUR";
+            long countUltimaHora = ExecuteCountQuery(conexaoConnection, queryUltimaHora);
+            AtualizarTextBox(txtResultados, $"Dados na última hora: {countUltimaHora}{Environment.NewLine}");
+
+            // Consulta para a primeira hora
+            string queryPrimeiraHora = $"SELECT COUNT(*) FROM {nomeTabela} WHERE data_hora >= (SELECT MIN(data_hora) FROM {nomeTabela}) AND data_hora < (SELECT MIN(data_hora) FROM {nomeTabela}) + INTERVAL 1 HOUR";
+            long countPrimeiraHora = ExecuteCountQuery(conexaoConnection, queryPrimeiraHora);
+            AtualizarTextBox(txtResultados, $"Dados na primeira hora: {countPrimeiraHora}{Environment.NewLine}");
+
+            // Consulta para o primeiro mês
+            string queryPrimeiroMes = $"SELECT COUNT(*) FROM {nomeTabela} WHERE data_hora >= (SELECT MIN(data_hora) FROM {nomeTabela}) AND data_hora < (SELECT MIN(data_hora) FROM {nomeTabela}) + INTERVAL 1 MONTH";
+            long countPrimeiroMes = ExecuteCountQuery(conexaoConnection, queryPrimeiroMes);
+            AtualizarTextBox(txtResultados, $"Dados no primeiro mês: {countPrimeiroMes}{Environment.NewLine}");
+
+            // Consulta para o primeiro ano
+            string queryPrimeiroAno = $"SELECT COUNT(*) FROM {nomeTabela} WHERE data_hora >= (SELECT MIN(data_hora) FROM {nomeTabela}) AND data_hora < (SELECT MIN(data_hora) FROM {nomeTabela}) + INTERVAL 1 YEAR";
+            long countPrimeiroAno = ExecuteCountQuery(conexaoConnection, queryPrimeiroAno);
+            AtualizarTextBox(txtResultados, $"Dados no primeiro ano: {countPrimeiroAno}{Environment.NewLine}");
+
+            // Consulta para os últimos 5 anos
+            string queryUltimosCincoAnos = $"SELECT COUNT(*) FROM {nomeTabela} WHERE data_hora >= NOW() - INTERVAL 5 YEAR";
+            long countUltimosCincoAnos = ExecuteCountQuery(conexaoConnection, queryUltimosCincoAnos);
+            AtualizarTextBox(txtResultados, $"Dados nos últimos 5 anos: {countUltimosCincoAnos}{Environment.NewLine}");
+        }
+
+        private long ExecuteCountQuery(MySqlConnection conexaoConnection, string query)
+        {
+            using (var cmd = new MySqlCommand(query, conexaoConnection))
+            {
+                return (long)cmd.ExecuteScalar();
+            }
+        }
+
+        private void cronometro_Tick(object sender, EventArgs e)
+        {
+            TimeSpan elapsed = DateTime.UtcNow - startTime;
+            lblCronometro.Text = $"{elapsed.Hours:D2}:{elapsed.Minutes:D2}:{elapsed.Seconds:D2}.{elapsed.Milliseconds:D3}";
+        }
+
+        private string GetConnectionString()
+        {
+            string server = "localhost";
+            string user = "root";
+            string pwd = "root";
+            string database = nomeBanco;
+            return $"server={server};user={user};pwd={pwd};database={database};SslMode=none;";
+        }
+
+        private void AtualizarLabel(Label label, string texto)
+        {
+            if (label.InvokeRequired)
+            {
+                label.Invoke((MethodInvoker)delegate { label.Text = texto; });
+            }
+            else
+            {
+                label.Text = texto;
+            }
+        }
+
+        private void AtualizarTextBox(System.Windows.Forms.TextBox textBox, string texto)
+        {
+            if (textBox.InvokeRequired)
+            {
+                textBox.Invoke((MethodInvoker)delegate { textBox.AppendText(texto); });
+            }
+            else
+            {
+                textBox.AppendText(texto);
+            }
+        }
+
+
+        private void AtualizarProgressBar(int valor)
+        {
+            if (progressBar.InvokeRequired)
+            {
+                progressBar.Invoke((MethodInvoker)delegate { progressBar.Value = valor; });
+            }
+            else
+            {
+                progressBar.Value = valor;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
